c9f96f5d16cee4e1b95355ae2b4bce71948df5bd
viral gujarati <viralgujarati02@gmail.com>
2021-03-08 16:49:38 +0530

    changes day3 commit

--- Modules/SQL/Day3/Viral Gujarati/Assignment/SQLQuery1.sql
+++ Modules/SQL/Day3/Viral Gujarati/Assignment/SQLQuery1.sql
@@ -3,33 +3,41 @@ USE AdventureWorks2012
 /*Write a query that displays the FirstName and the length of the FirstName for all employees whose name starts with the letters �A�, �J� or �M�. Give each column an appropriate label. Sort the results by the employees� FirstName   */
 
 SELECT FirstName,
-RIGHT(REPLICATE('$', 10) + LEFT(Salary, 10), 10) 
-FROM employees;
+LEN(FirstName) AS 'NameLength'
+FROM Employees
+WHERE FirstName LIKE ('A%') 
+	OR FirstName LIKE ('J%')
+	OR FirstName LIKE ('M%');
+
 
 
 /* Write a query to display the FirstName and Salary for all employees. Format the salary to be 10 characters long, left-padded with the $ symbol. Label the column SALARY.  */
-SELECT FirstName,Salary
-FROM Employees;
-SELECT FirstName,FORMAT(Salary,'####.####$') AS Salary
-FROM Employees;
+
+SELECT FirstName,
+RIGHT(REPLICATE('$', 10) + LEFT(Salary, 10), 10) 
+FROM employees;
 
 /* Write a query to display the employees with their code, first name, last name and hire date who hired either on seventh day of any month or seventh month in any year.  */
+
 SELECT EmployeeID,FirstName,LastName,HireDate
 FROM Employees
 WHERE DATEPART(DAY,HireDate)=7 OR DATEPART(MONTH,HireDate)=7;
 
 /* Write a query to display the length of first name for employees where last name contains character ‘c’ after 2nd position.  */
+
 SELECT LastName,
-	DATALENGTH(FirstName) As 'Length'
-	FROM Employees
-	WHERE CHARINDEX('c',LastName)>2;
+DATALENGTH(FirstName) As 'Length'
+FROM Employees
+WHERE CHARINDEX('c',LastName)>2;
 
 /*Write a query to extract the last 4 character of PhoneNumber.*/
+
 SELECT SUBSTRING(PhoneNumber,LEN(PhoneNumber)-3,4)
 FROM Employees;
 
 
 /* Write a query to update the portion of the PhoneNumber in the employees table, within the phone number the substring ‘124’ will be replaced by ‘999’.  */
+
 SELECT FirstName,PhoneNumber,SUBSTRING(PhoneNumber,LEN(PhoneNumber)-3,4) AS 'Last4Digits'
 FROM Employees;
 
@@ -38,6 +46,9 @@ SET PhoneNumber = REPLACE(PhoneNumber,'123','999');
 
 
 /*  Write a query to calculate the age in year. */
+
+
+
 /*  Write a query to get the distinct Mondays from HireDate in employees tables. */
 
 SELECT DISTINCT HireDate,DATEPART(WEEKDAY,HireDate)
@@ -46,12 +57,12 @@ WHERE DATEPART(WEEKDAY,HireDate) = 2;
 
 /* Write a query to get the FirstName and HireDate from Employees table where HireDate between ‘1987-06-01’ and ‘1987-07-30’  */
 
-SELECT FirstName,
-	HireDate
+SELECT FirstName,HireDate
 FROM Employees
 WHERE HireDate BETWEEN '1987-06-01' AND '1987-07-30';
 
 /*  Write a query to display the current date in the following format. */
+
 SELECT CONVERT(varchar, getdate(), 100);
 
 
